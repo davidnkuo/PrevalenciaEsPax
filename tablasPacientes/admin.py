2fd9e14fe9dac6fe615a13aa306a522c2533e508
davidnkuo <94645577+davidnkuo@users.noreply.github.com>
2021-12-15 13:40:54 -0300

    Add files via upload

---
+++ tablasPacientes/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from tablasPacientes.models import *
+# Register your models here.
+
+admin.site.register(datosPaciente)
+admin.site.register(datosEsPax)
+admin.site.register(datosHistorial)
+admin.site.register(datosEstudios)
---
+++ tablasPacientes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TablaspacientesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tablasPacientes'

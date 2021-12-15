e643a0b494fec57306f6bcf09700f4aa8556b8aa
davidnkuo <94645577+davidnkuo@users.noreply.github.com>
2021-12-15 13:41:33 -0300

    Delete Página Médicos directory

--- Página Médicos/tablasPacientes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from tablasPacientes.models import *
-# Register your models here.
-
-admin.site.register(datosPaciente)
-admin.site.register(datosEsPax)
-admin.site.register(datosHistorial)
-admin.site.register(datosEstudios)
